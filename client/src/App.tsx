a89da32eb704df3dd0af4ea39f09c2a951dde2cd
toanpham241997-cmyk <toanpham241997@gmail.com>
2026-02-07 20:43:06 +0700

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,6 @@
 import { Switch, Route } from "wouter";
+
+import Home from "@/pages/Home";
 import Auth from "@/pages/Auth";
 import FilesPage from "@/pages/Files";
 import Shop from "@/pages/Shop";
@@ -7,34 +9,38 @@ import ShopItem from "@/pages/ShopItem";
 import ChatPage from "@/pages/Chat";
 import OrdersPage from "@/pages/Orders";
 import OrderDetailPage from "@/pages/OrderDetail";
-import UploadPage from "@/pages/Upload"; // hoặc "@/pages/UploadPage" tùy tên file của bạn
+import UploadPage from "@/pages/Upload";
+import NotFound from "@/pages/not-found";
+
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import NotFound from "@/pages/not-found";
-import Home from "@/pages/Home";
+
 function Router() {
   return (
     <Switch>
-      <Switch>
-  <Route path="/" component={Home} />
+      {/* HOME – PHẢI ĐỨNG ĐẦU */}
+      <Route path="/" component={Home} />
 
-  <Route path="/shop" component={Shop} />
-  <Route path="/shop/p/:parentId" component={ShopParent} />
-  <Route path="/shop/i/:itemId" component={ShopItem} />
+      {/* SHOP */}
+      <Route path="/shop" component={Shop} />
+      <Route path="/shop/p/:parentId" component={ShopParent} />
+      <Route path="/shop/i/:itemId" component={ShopItem} />
 
-  <Route path="/auth" component={Auth} />
-  <Route path="/files" component={FilesPage} />
-  <Route path="/upload" component={UploadPage} />
-  <Route path="/chat" component={ChatPage} />
-  <Route path="/orders" component={OrdersPage} />
-  <Route path="/orders/:id" component={OrderDetailPage} />
+      {/* OTHER */}
+      <Route path="/auth" component={Auth} />
+      <Route path="/files" component={FilesPage} />
+      <Route path="/upload" component={UploadPage} />
+      <Route path="/chat" component={ChatPage} />
+      <Route path="/orders" component={OrdersPage} />
+      <Route path="/orders/:id" component={OrderDetailPage} />
 
-  <Route component={NotFound} />
-</Switch>
+      {/* 404 */}
+      <Route component={NotFound} />
+    </Switch>
   );
-        }
+}
 
 export default function App() {
   return (
